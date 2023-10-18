af41d8487104881b7679b8679a00a34433f7fc4f
Paul LaMendola <paulguy119@gmail.com>
2023-10-18 13:48:29 -0400

    Add more specificity to modes.
    Xorg seems to be more strict about modes matching now for some reason,
    to where 59.94 and 60 were too far apart, so to rectify this, 2 closer
    modes were needed.  This was made a bit more convenient with some
    additional variables.

--- set-tv.sh
+++ set-tv.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 MONITOR_MODE=2560x1440
+MONITOR_RATE=59.95
+TV_MODE=720x480
+TV_RATE=59.94
 SCRIPT_PATH=/home/paul/projects/parse-edid2/parse-edid2/parse-edid2/match-display.py
 
 MONITOR_CONNECTOR=$(${SCRIPT_PATH} name="lg ultragear+")
@@ -20,13 +23,13 @@ if [ x${TV_CONNECTOR} = x ]; then
 fi
 
 #Set main display to 60 hz so they can coexist.
-XRANDR=$(xrandr --output ${MONITOR_CONNECTOR} --mode ${MONITOR_MODE} --rate 60)
+XRANDR=$(xrandr --output ${MONITOR_CONNECTOR} --mode ${MONITOR_MODE} --rate ${MONITOR_RATE})
 if [ x${XRANDR} != x ]; then
     error ${XRANDR}
 fi
 
 #Enable TV
-XRANDR=$(xrandr --output ${TV_CONNECTOR} --mode 640x480 --right-of ${MONITOR_CONNECTOR})
+XRANDR=$(xrandr --output ${TV_CONNECTOR} --mode ${TV_MODE} --rate ${TV_RATE} --right-of ${MONITOR_CONNECTOR})
 if [ x${XRANDR} != x ]; then
     error ${XRANDR}
 fi
